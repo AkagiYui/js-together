24d993b70a3488aa585ac802403716fca695d408
akagiyui <akagiyui@yeah.net>
2025-11-04 21:05:45 +0800

    refactor(nottodo): move to vue

--- apps/nottodo/vite.config.ts
+++ apps/nottodo/vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  plugins: [vue()],
 })
